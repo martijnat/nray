68e3ddac6b8ea971fc80fe041246aa75e062d9dc
Martijn Terpstra <bigmartijn@gmail.com>
2017-10-24 19:55:42 +0200

    added more classes

--- ray_math.py
+++ ray_math.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-def sqrt(x,error=(2**-100)):
+
+def sqrt(x, error=(2**-100)):
     r = 0
     step = x
-    while abs(r*r-x)>error:
-        if abs((r+step)*(r+step)-x)<abs(r*r-x):
+    while abs(r * r - x) > error:
+        if abs((r + step) * (r + step) - x) < abs(r * r - x):
             r += step
-        elif abs((r-step)*(r-step)-x)<abs(r*r-x):
+        elif abs((r - step) * (r - step) - x) < abs(r * r - x):
             r -= step
         else:
             step = step * 0.5
     return r
+
 
 class vec3():
     def __init__(self, x=0.0, y=0.0, z=0.0):
@@ -38,30 +40,54 @@ class vec3():
         return vec3(scalar * self.x,
                     scalar * self.y,
                     scalar * self.z)
-    def dot(self,other):
-        return sum((self.x*other.x,
-                    self.y*other.y,
-                    self.z*other.z))
+
+    def dot(self, other):
+        return sum((self.x * other.x,
+                    self.y * other.y,
+                    self.z * other.z))
 
     def normalized(self):
         s = abs(self)
-        if s<=0:
+        if s <= 0:
             return vec3()
-        return vec3(self.x/sqrt(s),
-                    self.y/sqrt(s),
-                    self.z/sqrt(s))
+        return vec3(self.x / sqrt(s),
+                    self.y / sqrt(s),
+                    self.z / sqrt(s))
 
     def __abs__(self):
         return self.dot(self)
 
 
+class ray():
+    def __init__(self, o, d):
+        self.o = o
+        self.d = d.normalized()
+
+
+class sphere():
+    "x²+y²+z² = r"
+
+    def __init__(self, x, y, z, r, color):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.r = r
+        self.color = color
+
+
+class light():
+    def __init__(self, pos, color):
+        self.pos = pos
+        self.color = color
+
+
 if __name__ == "__main__":
-    v1 = vec3(1,0,0)
-    v2 = vec3(1,2,3)
-    s  = 3
-    v3 = v2*s
+    v1 = vec3(1, 0, 0)
+    v2 = vec3(1, 2, 3)
+    s = 3
+    v3 = v2 * s
 
     assert abs(v1) == 1.0
     assert abs(v2.normalized()) == 1.0
-    assert abs(v1+v2) == abs(v2+v1)
-    assert s*s*abs(v2) == abs(v3)
+    assert abs(v1 + v2) == abs(v2 + v1)
+    assert s * s * abs(v2) == abs(v3)
